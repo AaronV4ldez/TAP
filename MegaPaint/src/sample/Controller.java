2b0f4ebe926b31986f5356a89ec94ecf577ff386
AaronV4ldez <73905734+AaronV4ldez@users.noreply.github.com>
2021-03-17 20:26:19 -0700

    jala la pluma xD

--- MegaPaint/src/sample/Controller.java
+++ MegaPaint/src/sample/Controller.java
@@ -1,4 +1,22 @@
 package sample;
 
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.paint.Color;
+
 public class Controller {
+    @FXML Canvas lienzo;
+    GraphicsContext context;
+    @FXML protected void initialize(){
+        context=lienzo.getGraphicsContext2D();
+        context.setFill(Color.BLACK);
+
+    }
+    public void arrastrar (MouseEvent event){
+    context.fillOval(event.getX(), event.getY(), 10 , 10);
+
+    }
+
 }
--- MegaPaint/src/sample/Main.java
+++ MegaPaint/src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Aarón Valdez Garcia");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1032, 751));
         primaryStage.show();
     }
 
