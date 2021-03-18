a320ee7629ad75b9d28ee1b9f0006e076860cc89
AaronV4ldez <73905734+AaronV4ldez@users.noreply.github.com>
2021-03-17 21:18:51 -0700

    ya jala todo ñwñ

--- MegaPaint/src/sample/Controller.java
+++ MegaPaint/src/sample/Controller.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.ColorPicker;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
@@ -12,20 +13,24 @@ import javafx.scene.paint.Color;
 public class Controller {
     @FXML Canvas lienzo;
     GraphicsContext context;
+    @FXML ColorPicker colorPicker;
+    Color colorPincel=Color.BLACK;
     @FXML protected void initialize(){
         context=lienzo.getGraphicsContext2D();
         context.setFill(Color.BLACK);
 
     }
     public void arrastrar (MouseEvent event){
+        context.setFill(colorPincel);
     context.fillOval(event.getX(), event.getY(), 10 , 10);
-
+    }
+    public void cambiarColor (ActionEvent event){
+        colorPincel= colorPicker.getValue();
     }
 
     public void borrar (ActionEvent event){
         context.setFill(Color.WHITESMOKE);
         context.fillRect(0,0, lienzo.getHeight(), lienzo.getWidth());
-
     }
 
 }
